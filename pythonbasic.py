313c073e6a323c129d3d8d70c280bfe3e3fd9ab0
chetanphulmante <chetanphulmante123@gmail.com>
2024-05-07 01:21:09 +0530

    first commit

---
+++ pythonbasic.py
@@ -0,0 +1,18 @@
+print("hello\n\tworld")
+std:print(20+30)
+x=15
+y=30
+z=x+y
+print(z)
+a=eval(input())
+b=eval(input())
+c=a+b
+print(c)
+if a==10:
+# {
+    print("even")
+# }
+elif a==11:
+# {
+    print("odd")  
+# } 
